e58e1047e87f6db25460da5a4f80eafd2061ca64
nixpig <143995476+nixpig@users.noreply.github.com>
2023-11-04 07:26:57 +0000

    chore: update db scheme sql

--- nixpigweb.sql
+++ nixpigweb.sql
@@ -21,30 +21,24 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: posts; Type: TABLE; Schema: public; Owner: postgres
+-- Name: category_; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.posts (
+CREATE TABLE public.category_ (
     id integer NOT NULL,
-    title character varying(255) NOT NULL,
-    body text,
-    views integer DEFAULT 0 NOT NULL,
-    slug character varying(255) NOT NULL,
-    published boolean DEFAULT false NOT NULL,
-    published_at time without time zone,
-    updated_at time without time zone,
-    user_id integer
+    name_ character varying(50),
+    template_id_ integer
 );
 
 
-ALTER TABLE public.posts OWNER TO postgres;
+ALTER TABLE public.category_ OWNER TO postgres;
 
 --
--- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: category__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public.posts ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.posts_id_seq
+ALTER TABLE public.category_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.category__id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -54,27 +48,24 @@ ALTER TABLE public.posts ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
+-- Name: category_meta_; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.users (
+CREATE TABLE public.category_meta_ (
     id integer NOT NULL,
-    username character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    is_admin boolean DEFAULT false NOT NULL,
-    password character varying(255),
-    registered_at time without time zone
+    category_id_ integer,
+    meta_id_ integer
 );
 
 
-ALTER TABLE public.users OWNER TO postgres;
+ALTER TABLE public.category_meta_ OWNER TO postgres;
 
 --
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: category_meta__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.users_id_seq
+ALTER TABLE public.category_meta_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.category_meta__id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -84,81 +75,424 @@ ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Name: config_; Type: TABLE; Schema: public; Owner: postgres
 --
 
-COPY public.posts (id, title, body, views, slug, published, published_at, updated_at, user_id) FROM stdin;
-\.
+CREATE TABLE public.config_ (
+    id integer NOT NULL,
+    name_ character varying(50),
+    value_ character varying(255)
+);
 
 
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.users (id, username, email, is_admin, password, registered_at) FROM stdin;
-\.
-
+ALTER TABLE public.config_ OWNER TO postgres;
 
 --
--- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: config__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+ALTER TABLE public.config_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.config__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 
 
 --
--- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: meta_; Type: TABLE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.posts
-    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
+CREATE TABLE public.meta_ (
+    id integer NOT NULL,
+    name_ character varying(50),
+    value_ character varying(255)
+);
 
 
---
--- Name: posts posts_slug_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.posts
-    ADD CONSTRAINT posts_slug_key UNIQUE (slug);
-
+ALTER TABLE public.meta_ OWNER TO postgres;
 
 --
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: meta__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+ALTER TABLE public.meta_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.meta__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 
 
 --
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: page_; Type: TABLE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_username_key UNIQUE (username);
+CREATE TABLE public.page_ (
+    id integer NOT NULL,
+    title_ character varying(255) DEFAULT ''::character varying NOT NULL,
+    body_ text DEFAULT ''::text NOT NULL,
+    slug_ character varying(255) NOT NULL,
+    status_ character varying(10) DEFAULT 'draft'::character varying NOT NULL,
+    created_at_ timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    published_at_ timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at_ timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    user_id_ integer,
+    category_id_ integer
+);
+
+
+ALTER TABLE public.page_ OWNER TO postgres;
+
+--
+-- Name: page__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.page_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.page__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 
 
 --
--- Name: posts posts_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: page_meta_; Type: TABLE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.posts
-    ADD CONSTRAINT posts_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+CREATE TABLE public.page_meta_ (
+    id integer NOT NULL,
+    page_id_ integer,
+    meta_id_ integer
+);
+
+
+ALTER TABLE public.page_meta_ OWNER TO postgres;
+
+--
+-- Name: page_meta__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.page_meta_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.page_meta__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: post_; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.post_ (
+    id integer NOT NULL,
+    title_ character varying(255) DEFAULT ''::character varying NOT NULL,
+    subtitle_ character varying(255) DEFAULT ''::character varying NOT NULL,
+    body_ text DEFAULT ''::text NOT NULL,
+    slug_ character varying(255) DEFAULT ''::character varying NOT NULL,
+    status_ character varying(10) DEFAULT 'draft'::character varying NOT NULL,
+    created_at_ timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    published_at_ timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at_ timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    user_id_ integer,
+    category_id_ integer
+);
+
+
+ALTER TABLE public.post_ OWNER TO postgres;
+
+--
+-- Name: post__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.post_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.post__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: template_; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.template_ (
+    id integer NOT NULL,
+    name_ character varying(50),
+    tmpl_ character varying(255)
+);
+
+
+ALTER TABLE public.template_ OWNER TO postgres;
+
+--
+-- Name: template__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.template_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.template__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: user_; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.user_ (
+    id integer NOT NULL,
+    username_ character varying(50) NOT NULL,
+    email_ character varying(50) NOT NULL,
+    is_admin_ boolean DEFAULT false NOT NULL,
+    password_ character varying(255) NOT NULL,
+    registered_at_ time without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    last_login_ time without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    role_ character varying(10) DEFAULT 'reader'::character varying NOT NULL,
+    profile_ character varying(255) DEFAULT ''::character varying NOT NULL
+);
+
+
+ALTER TABLE public.user_ OWNER TO postgres;
+
+--
+-- Name: user__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.user_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.user__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: user_meta_; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.user_meta_ (
+    id integer NOT NULL,
+    user_id_ integer,
+    meta_id_ integer
+);
+
+
+ALTER TABLE public.user_meta_ OWNER TO postgres;
+
+--
+-- Name: user_meta__id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.user_meta_ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.user_meta__id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: category_ category__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category_
+    ADD CONSTRAINT category__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: category_meta_ category_meta__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category_meta_
+    ADD CONSTRAINT category_meta__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: config_ config__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.config_
+    ADD CONSTRAINT config__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: meta_ meta__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.meta_
+    ADD CONSTRAINT meta__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: page_ page__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.page_
+    ADD CONSTRAINT page__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: page_meta_ page_meta__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.page_meta_
+    ADD CONSTRAINT page_meta__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: post_ post__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.post_
+    ADD CONSTRAINT post__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: template_ template__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.template_
+    ADD CONSTRAINT template__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user_ user__email__key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_
+    ADD CONSTRAINT user__email__key UNIQUE (email_);
+
+
+--
+-- Name: user_ user__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_
+    ADD CONSTRAINT user__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user_ user__username__key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_
+    ADD CONSTRAINT user__username__key UNIQUE (username_);
+
+
+--
+-- Name: user_meta_ user_meta__pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_meta_
+    ADD CONSTRAINT user_meta__pkey PRIMARY KEY (id);
+
+
+--
+-- Name: category_ category__template_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category_
+    ADD CONSTRAINT category__template_id__fkey FOREIGN KEY (template_id_) REFERENCES public.template_(id);
+
+
+--
+-- Name: category_meta_ category_meta__category_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category_meta_
+    ADD CONSTRAINT category_meta__category_id__fkey FOREIGN KEY (category_id_) REFERENCES public.category_(id);
+
+
+--
+-- Name: category_meta_ category_meta__meta_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category_meta_
+    ADD CONSTRAINT category_meta__meta_id__fkey FOREIGN KEY (meta_id_) REFERENCES public.meta_(id);
+
+
+--
+-- Name: page_ page__category_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.page_
+    ADD CONSTRAINT page__category_id__fkey FOREIGN KEY (category_id_) REFERENCES public.category_(id);
+
+
+--
+-- Name: page_ page__user_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.page_
+    ADD CONSTRAINT page__user_id__fkey FOREIGN KEY (user_id_) REFERENCES public.user_(id);
+
+
+--
+-- Name: page_meta_ page_meta__meta_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.page_meta_
+    ADD CONSTRAINT page_meta__meta_id__fkey FOREIGN KEY (meta_id_) REFERENCES public.meta_(id);
+
+
+--
+-- Name: page_meta_ page_meta__page_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.page_meta_
+    ADD CONSTRAINT page_meta__page_id__fkey FOREIGN KEY (page_id_) REFERENCES public.page_(id);
+
+
+--
+-- Name: post_ post__category_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.post_
+    ADD CONSTRAINT post__category_id__fkey FOREIGN KEY (category_id_) REFERENCES public.category_(id);
+
+
+--
+-- Name: post_ post__user_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.post_
+    ADD CONSTRAINT post__user_id__fkey FOREIGN KEY (user_id_) REFERENCES public.user_(id);
+
+
+--
+-- Name: user_meta_ user_meta__meta_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_meta_
+    ADD CONSTRAINT user_meta__meta_id__fkey FOREIGN KEY (meta_id_) REFERENCES public.meta_(id);
+
+
+--
+-- Name: user_meta_ user_meta__user_id__fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_meta_
+    ADD CONSTRAINT user_meta__user_id__fkey FOREIGN KEY (user_id_) REFERENCES public.user_(id);
 
 
 --
