71ff6e0619bdf37fb1ded19aa7de7ee69b3cb0c8
nixpig <143995476+nixpig@users.noreply.github.com>
2023-12-10 11:31:28 +0000

    feat: handle admin login

--- web/app/src/pages/Login.tsx
+++ web/app/src/pages/Login.tsx
@@ -1,12 +1,68 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../provider/authProvider";
+import axios from "axios";
+import { useState } from "react";
+
+const login = async (
+  e: any,
+  username: string,
+  password: string,
+  setToken: any
+) => {
+  e.preventDefault();
+  console.log(username, password);
+
+  try {
+    let res = await axios.post("https://nixpig.dev/api/auth/login", {
+      username,
+      password,
+    });
+
+    console.log(res.data);
+    setToken(res.data.data.token);
+  } catch (e) {
+    console.error("login failed...");
+    alert("Login failed");
+  }
+};
 
 export const Login = () => {
-  const { token } = useAuth();
+  const { token, setToken } = useAuth();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   if (token) {
     return <Navigate to="/dashboard" />;
   }
 
-  return <div>Login screen</div>;
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input id="username" onChange={(e) => setUsername(e.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            id="login-btn"
+            onClick={(e) => login(e, username, password, setToken)}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
