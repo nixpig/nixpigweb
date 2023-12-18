db806d42dafcd526a7ccccd8748af44e9586e362
nixpig <143995476+nixpig@users.noreply.github.com>
2023-12-18 06:34:23 +0000

    chore: tidy a few bits up

--- internal/web/handlers/index.go
+++ internal/web/handlers/index.go
@@ -15,9 +15,19 @@ func IndexHandler(c *fiber.Ctx) error {
 		})
 	}
 
+	posts, err := queries.GetContentByType("post")
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).Render("500", fiber.Map{
+			"PageTitle": "500 - Internal Server Error",
+			"SiteName":  config.Get("SITE_NAME"),
+		})
+	}
+
 	return c.Render("index", fiber.Map{
 		"Pages":        pages,
+		"Posts":        posts,
 		"SiteName":     config.Get("SITE_NAME"),
+		"PageTitle":    "Welcome to nixpig.dev",
 		"PageSubtitle": "Diary of a front-ender learning Rust, Go and other back-end tech...",
 	})
 }
